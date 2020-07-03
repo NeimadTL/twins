ed973dfc1b57518382fdbbd83197372f9e82bca4
neimad971 <damientalbot26@gmail.com>
2020-07-03 17:25:55 +0200

    adds comment for algorithm

--- checkable.rb
+++ checkable.rb
@@ -1,5 +1,10 @@
 module Checkable
 
+  # we assume the strings are correctly swapped then we iterate over each character with
+  # an index. When the current index is even, unless the current character on string A
+  # is placed on an even index on string B AND the strings are still considered correctly
+  # swapped (accumulator pattern), we break out of the iteration by returning false.
+  # Respectively, the same rule is applied when it is an odd index
   def correctly_swapped?(str_a, str_b)
     correctly_swapped = true
     str_a.chars.each_with_index do |char, index|
